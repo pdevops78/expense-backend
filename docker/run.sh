489683ce9c430e656853cbfabc09ac2d83fa5eaf
Your Name <vemasani.pavani@gmail.com>
2025-08-16 23:06:21 +0530

    backend image

---
+++ docker/run.sh
@@ -0,0 +1,18 @@
+if [ -z "$DB_HOST" ]; then
+  echo DB_HOST is missing
+  exit 1
+fi
+
+if [ -z "$NEW_RELIC_APP_NAME" ]; then
+  echo NEW_RELIC_APP_NAME is missing
+  exit 1
+fi
+
+if [ -z  "$NEW_RELIC_LICENSE_KEY"]; then
+    echo NEW_RELIC_LICENSE_KEY is missing
+    exit 1
+fi
+
+
+node -r newrelic /app/index.js
+
