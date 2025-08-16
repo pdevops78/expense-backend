b651f7d7fbacc5951d2578201de2e91184c449c2
Your Name <vemasani.pavani@gmail.com>
2025-08-17 00:39:08 +0530

    vault image

--- run.sh
+++ run.sh
@@ -1,20 +1,4 @@
 source /tmp/data
-
-if [ -z "${DB_HOST}" ]; then
-  echo DB_HOST is missing
-  exit 1
-fi
-
-if [ -z "${NEW_RELIC_APP_NAME}" ]; then
-  echo NEW_RELIC_APP_NAME is missing
-  exit 1
-fi
-
-if [ -z  "${NEW_RELIC_LICENSE_KEY}" ]; then
-    echo NEW_RELIC_LICENSE_KEY is missing
-    exit 1
-fi
-
-
+cat /tmp/data
 node -r newrelic /app/index.js
 
