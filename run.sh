b36030abefac2b239dbfa6559bc9914ab2fad6ab
Your Name <vemasani.pavani@gmail.com>
2025-09-10 06:27:46 +0530

    Revert "helm chart"
    This reverts commit 52934e17

--- run.sh
+++ run.sh
@@ -1,22 +1,5 @@
 source /data/secrets.txt
 cat /data/secrets.txt
 
-echo "expense-backend"
-
-if [ -z "${DB_HOST}" ]; then
-   echo environment variable DB_HOST is missing
-   exit 1
-fi
-
-if [ -z "${NEW_RELIC_APP_NAME}" ]; then
-  echo environment variable NEW_RELIC_APP_NAME is missing
-  exit 1
-fi
-
-if [ -z  "${NEW_RELIC_LICENSE_KEY}" ]; then
-    echo environment variable NEW_RELIC_LICENSE_KEY is missing
-    exit 1
-fi
-
 node -r newrelic /app/index.js
 
