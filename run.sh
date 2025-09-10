0489f027e4a5a48e3581bc23eebae33bc2cfacd0
Your Name <vemasani.pavani@gmail.com>
2025-09-10 08:03:22 +0530

    backend

--- run.sh
+++ run.sh
@@ -1,10 +1,22 @@
-echo "Helo"
 source /data/secrets.txt
 cat /data/secrets.txt
 
-if [ -z "${DB_HOST}" ]; then
-   echo Environment variable is missing
-   echo $DB_HOST
-   exit 1;
+if [ -z "$DB_HOST" ]; then
+  echo Environment Variable DB_HOST is mising
+  exit 1
 fi
-node /app/index.js
+
+if [ -z "$NEW_RELIC_APP_NAME" ]; then
+  echo Environment Variable NEW_RELIC_APP_NAME is mising
+  exit 1
+fi
+
+if [ -z "$NEW_RELIC_LICENSE_KEY" ]; then
+  echo Environment Variable NEW_RELIC_LICENSE_KEY is mising
+  exit 1
+fi
+
+node -r newrelic /app/index.js
+
+
+
