364643e9a2b37b6153e6213fe2b3566391529ca1
Your Name <vemasani.pavani@gmail.com>
2025-09-10 07:27:55 +0530

    backend

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
+echo "Helo"
 source /data/secrets.txt
 cat /data/secrets.txt
 
+if [ -z "${DB_HOST}" ]; then
+   echo Environment variable is missing
+   echo $DB_HOST
+   exit 1;
+fi
 node /app/index.js
